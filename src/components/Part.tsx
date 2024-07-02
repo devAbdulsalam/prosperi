1960f0d4e4920bf74cc898a675c41af33ae4e2ab
devabdulsalam <ammuftau74@gmail.com>
2024-07-02 21:15:36 +0100

    fix particle errorr

--- src/components/Part.tsx
+++ src/components/Part.tsx
@@ -43,7 +43,7 @@
 
 // export default App;
 
-import { useEffect, useState, useMemo } from 'react';
+import { useEffect, useState } from 'react';
 import Particles, { initParticlesEngine } from '@tsparticles/react';
 // import type { Container, Engine } from "@tsparticles/engine";
 // import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
@@ -73,157 +73,157 @@ const App = () => {
 	//     console.log(container);
 	// };
 
-	const options = useMemo(
-		() => ({
-			fullScreen: {
-				enable: true,
-				zIndex: -1,
-			},
-			background: {
-				color: {
-					value: 'white',
-				},
-			},
-			fpsLimit: 120,
-			interactivity: {
-				events: {
-					onClick: {
-						enable: true,
-						mode: 'push',
-					},
-					onHover: {
-						enable: true,
-						mode: 'repulse',
-					},
-				},
-				modes: {
-					push: {
-						quantity: 4,
-					},
-					repulse: {
-						distance: 200,
-						duration: 0.4,
-					},
-				},
-			},
-			particles: {
-				color: {
-					value: '#0d47a1',
-				},
-				links: {
-					color: '#0d47a1',
-					distance: 150,
-					enable: true,
-					opacity: 0.5,
-					width: 1,
-				},
-				move: {
-					direction: 'topLeft',
-					enable: true,
-					outModes: {
-						default: 'bounce',
-					},
-					random: false,
-					speed: 6,
-					straight: false,
-				},
-				number: {
-					density: {
-						enable: true,
-					},
-					value: 80,
-				},
-				opacity: {
-					value: 0.5,
-				},
-				shape: {
-					type: 'circle',
-				},
-				size: {
-					value: { min: 1, max: 5 },
-				},
-			},
-			detectRetina: true,
-		}),
-		[]
-	);
+	// const options = useMemo(
+	// 	() => ({
+	// 		fullScreen: {
+	// 			enable: true,
+	// 			zIndex: -1,
+	// 		},
+	// 		background: {
+	// 			color: {
+	// 				value: 'white',
+	// 			},
+	// 		},
+	// 		fpsLimit: 120,
+	// 		interactivity: {
+	// 			events: {
+	// 				onClick: {
+	// 					enable: true,
+	// 					mode: 'push',
+	// 				},
+	// 				onHover: {
+	// 					enable: true,
+	// 					mode: 'repulse',
+	// 				},
+	// 			},
+	// 			modes: {
+	// 				push: {
+	// 					quantity: 4,
+	// 				},
+	// 				repulse: {
+	// 					distance: 200,
+	// 					duration: 0.4,
+	// 				},
+	// 			},
+	// 		},
+	// 		particles: {
+	// 			color: {
+	// 				value: '#0d47a1',
+	// 			},
+	// 			links: {
+	// 				color: '#0d47a1',
+	// 				distance: 150,
+	// 				enable: true,
+	// 				opacity: 0.5,
+	// 				width: 1,
+	// 			},
+	// 			move: {
+	// 				direction: 'topLeft',
+	// 				enable: true,
+	// 				outModes: {
+	// 					default: 'bounce',
+	// 				},
+	// 				random: false,
+	// 				speed: 6,
+	// 				straight: false,
+	// 			},
+	// 			number: {
+	// 				density: {
+	// 					enable: true,
+	// 				},
+	// 				value: 80,
+	// 			},
+	// 			opacity: {
+	// 				value: 0.5,
+	// 			},
+	// 			shape: {
+	// 				type: 'circle',
+	// 			},
+	// 			size: {
+	// 				value: { min: 1, max: 5 },
+	// 			},
+	// 		},
+	// 		detectRetina: true,
+	// 	}),
+	// 	[]
+	// );
 
 	return (
 		<>
 			{init && (
 				<Particles
 					id="tsparticles"
-					options={options}
+					// options={options}
 					// particlesLoaded={particlesLoaded}
-					// options={{
-					// 	background: {
-					// 		color: {
-					// 			value: '#0d47a1',
-					// 		},
-					// 	},
-					// 	fpsLimit: 120,
-					// 	interactivity: {
-					// 		events: {
-					// 			onClick: {
-					// 				enable: true,
-					// 				mode: 'push',
-					// 			},
-					// 			onHover: {
-					// 				enable: true,
-					// 				mode: 'repulse',
-					// 			},
-					// 			// resize: true,
-					// 		},
-					// 		modes: {
-					// 			push: {
-					// 				quantity: 4,
-					// 			},
-					// 			repulse: {
-					// 				distance: 200,
-					// 				duration: 0.4,
-					// 			},
-					// 		},
-					// 	},
-					// 	particles: {
-					// 		color: {
-					// 			value: '#ffffff',
-					// 		},
-					// 		links: {
-					// 			color: '#ffffff',
-					// 			distance: 150,
-					// 			enable: true,
-					// 			opacity: 0.5,
-					// 			width: 1,
-					// 		},
-					// 		move: {
-					// 			direction: 'none',
-					// 			enable: true,
-					// 			outModes: {
-					// 				default: 'bounce',
-					// 			},
-					// 			random: false,
-					// 			speed: 6,
-					// 			straight: false,
-					// 		},
-					// 		number: {
-					// 			density: {
-					// 				enable: true,
-					// 				area: 800,
-					// 			},
-					// 			value: 80,
-					// 		},
-					// 		opacity: {
-					// 			value: 0.5,
-					// 		},
-					// 		shape: {
-					// 			type: 'circle',
-					// 		},
-					// 		size: {
-					// 			value: { min: 1, max: 5 },
-					// 		},
-					// 	},
-					// 	detectRetina: true,
-					// }}
+					options={{
+						background: {
+							color: {
+								value: 'white',
+							},
+						},
+						fpsLimit: 120,
+						interactivity: {
+							events: {
+								onClick: {
+									enable: true,
+									mode: 'push',
+								},
+								onHover: {
+									enable: true,
+									mode: 'repulse',
+								},
+								// resize: true,
+							},
+							modes: {
+								push: {
+									quantity: 4,
+								},
+								repulse: {
+									distance: 200,
+									duration: 0.4,
+								},
+							},
+						},
+						particles: {
+							color: {
+								value: '#0d47a1',
+							},
+							links: {
+								color: '#0d47a1',
+								distance: 150,
+								enable: true,
+								opacity: 0.5,
+								width: 1,
+							},
+							move: {
+								direction: 'none',
+								enable: true,
+								outModes: {
+									default: 'bounce',
+								},
+								random: false,
+								speed: 6,
+								straight: false,
+							},
+							number: {
+								density: {
+									enable: true,
+									// area: 800,
+								},
+								value: 80,
+							},
+							opacity: {
+								value: 0.5,
+							},
+							shape: {
+								type: 'circle',
+							},
+							size: {
+								value: { min: 1, max: 5 },
+							},
+						},
+						detectRetina: true,
+					}}
 				/>
 			)}
 		</>
