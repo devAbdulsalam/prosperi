843612e2cb696f4922bd4fc5c5025c02b4d22523
devabdulsalam <ammuftau74@gmail.com>
2024-07-04 13:04:50 +0100

    add scroll to all pages

--- src/App.tsx
+++ src/App.tsx
@@ -10,22 +10,26 @@ import Ambassadors from './pages/Ambassadors';
 import Blog from './pages/Blog';
 import TermofUse from './pages/TermofUse';
 import PrivacyPolicy from './pages/PrivacyPolicy';
+import ScrollToTop from './components/ScrollToTop';
 
 const App = () => {
 	return (
-		<Routes>
-			<Route path="/" element={<Index />} />
-			<Route path="/career" element={<Career />} />
-			<Route path="/about" element={<About />} />
-			<Route path="/ambassadors" element={<Ambassadors />} />
-			<Route path="/privacy-policy" element={<PrivacyPolicy />} />
-			<Route path="/term-of-use" element={<TermofUse />} />
-			<Route path="/blog" element={<Blog />} />
-			<Route path="/license" element={<License />} />
-			<Route path="/user-guide" element={<UserGuide />} />
-			<Route path="/faq" element={<Faq />} />
-			<Route path="*" element={<NotFound />} />
-		</Routes>
+		<>
+			<ScrollToTop />
+			<Routes>
+				<Route path="/" element={<Index />} />
+				<Route path="/career" element={<Career />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/ambassadors" element={<Ambassadors />} />
+				<Route path="/privacy-policy" element={<PrivacyPolicy />} />
+				<Route path="/term-of-use" element={<TermofUse />} />
+				<Route path="/blog" element={<Blog />} />
+				<Route path="/license" element={<License />} />
+				<Route path="/user-guide" element={<UserGuide />} />
+				<Route path="/faq" element={<Faq />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+		</>
 	);
 };
 
