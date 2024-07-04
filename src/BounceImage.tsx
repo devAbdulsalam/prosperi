52f1bd78ebd5480c9a5b3cf010450e2e187583b9
devabdulsalam <ammuftau74@gmail.com>
2024-07-04 21:36:17 +0100

    remove bounce from robot

--- src/BounceImage.tsx
+++ src/BounceImage.tsx
@@ -1,19 +1,19 @@
-import { useSpring, animated } from '@react-spring/web';
+import { animated } from '@react-spring/web';
 
 const BouncingImage = ({ src, alt }: { src: string; alt: string }) => {
-	const styles = useSpring({
-		from: { transform: 'translateY(0px)' },
-		to: [{ transform: 'translateY(-30px)' }, { transform: 'translateY(0px)' }],
-		config: { duration: 500 },
-		loop: true,
-	});
+	// const styles = useSpring({
+	// 	from: { transform: 'translateY(0px)' },
+	// 	to: [{ transform: 'translateY(-30px)' }, { transform: 'translateY(0px)' }],
+	// 	config: { duration: 500 },
+	// 	loop: true,
+	// });
 
 	return (
 		<animated.img
 			src={src}
 			alt={alt}
 			className="object-fit object-center z-10"
-			style={styles}
+			// style={styles}
 		/>
 	);
 };
