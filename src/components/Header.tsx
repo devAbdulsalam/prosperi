f7830c639a9bc0dddf53188d5b67fc96ddbd7664
devabdulsalam <ammuftau74@gmail.com>
2024-07-09 16:06:26 +0100

    fix testimonial slide

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,18 +15,25 @@ const Header = () => {
 	return (
 		<header
 			id="home"
-			className="bg-transparent text-white pt-20 relative w-full overflow-hidden"
+			className="bg-transparent text-white pt-20 relative w-full overflow-hidden isolate"
 		>
 			<img
 				src={layer1}
 				alt="appstore"
-				className="absolute top-0 left-0 object-cover object-center w-full h-full z-10 bg-[#06002B]"
+				className="absolute top-0 left-0 object-cover object-center w-full h-full z-10"
 			/>
 			<img
 				src={layer2}
 				alt="appstore"
 				className="absolute top-0 left-0 object-cover object-center w-full h-full z-10 "
 			/>
+
+			<div
+				className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
+				aria-hidden="true"
+			>
+				<div className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#7274e6] to-[#5856e2] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem] clipPath"></div>
+			</div>
 			<div className="md:container mx-auto">
 				<div className="relative w-full h-[320px]  md:h-[420px]">
 					<div className="absolute md:right-44 lg:right-56 top-40 h-full hidden lg:block z-10">
